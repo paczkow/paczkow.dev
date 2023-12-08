d87878b8911147388ba443aac723575a04cc0dea
Michał Paczków <michal.paczkow@gmail.com>
2023-12-08 17:57:33 +0100

    Add astro-paper-v3 template

---
+++ src/config.ts
@@ -0,0 +1,46 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://paczkow.github.io", // replace this with your deployed domain
+  author: "Michał Paczków",
+  desc: "Personal blog. Write about JavaScript/Typescript, shipping fullstack stuff 🚀 and knowledge management using Obsidian",
+  title: "paczkow.dev",
+  lightAndDarkMode: true,
+  postPerPage: 5,
+};
+
+export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/paczkow",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/paczkow/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/_paczkow",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:michal.paczkow+paczkow.dev@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
+  },
+];
---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,13 @@
+---
+author: paczkow
+pubDatetime: 2023-09-25T10:25:54.547Z
+title: Hello World!
+postSlug: hello-world
+featured: true
+ogImage: https://github.com/satnaing/astro-paper/assets/53733092/1ef0cf03-8137-4d67-ac81-84a032119e3a
+tags:
+  - release
+description: "Something about me"
+---
+
+Just for test purposes
