0fdfa987667c082a90828c0a427c05d5aab51af0
Michał Paczków <michal.paczkow@gmail.com>
2023-12-09 12:53:03 +0100

    feat: add reading time to posts

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, description } = frontmatter;
+  const { title, pubDatetime, description, readingTime } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
@@ -28,7 +28,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
           <h3 {...headerProps}>{title}</h3>
         )}
       </a>
-      <Datetime datetime={pubDatetime} />
+      <Datetime datetime={pubDatetime} readingTime={readingTime} />
       <p>{description}</p>
     </li>
   );
